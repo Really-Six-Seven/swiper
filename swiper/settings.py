4a3b693352d550ea9a1f090884184ec16f76b898
Really-Six-Seven <1308986733@qq.com>
2019-04-12 12:00:31 +0800

    user ok,add social

--- swiper/settings.py
+++ swiper/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     # 'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'social',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'Asia/shanghai'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT='media/uploads/'
--- user/api.py
+++ user/api.py
@@ -1,8 +1,10 @@
 from lib.sms import send_sms
-from lib.http import render_json
+from lib.render_json import render_json
 from common import errors
 from django.core.cache import cache
 from common import keys
+from user import logics
+from user.forms import ProfileFrom
 from user.models import User
 
 
@@ -27,8 +29,10 @@ def submit_vcode(request):
     phone = request.POST.get('phone')
     # 取到发到手机的验证码
     vcode = request.POST.get('vcode')
+    print(phone)
     # 取到缓存中的验证码
     cache_vcode = cache.get(keys.VCODE_KEY % phone)
+    print(cache_vcode)
 
     #对比验证码是否一致
     if vcode == cache_vcode:
@@ -39,17 +43,49 @@ def submit_vcode(request):
     else:
         return render_json('verify code error',errors.VCODE_ERROR)
 
+
 def get_profile(request):
     """获取个人资料"""
-    pass
+    uid = request.session.get('uid')
+    user = User.objects.get(id=uid)
+    profile = user.profile
 
+    return render_json(profile.to_string())
 
 def set_profile(request):
     """修改个人资料"""
-    pass
+    # 判断是否是post请求
+    if not request.method == "POST":
+        return render_json('request method error', errors.REQUEST_ERROR)
 
+    uid = request.session.get('uid')
+    profile_form = ProfileFrom(request.POST)
 
+    if profile_form.is_valid():
+        profile = profile_form.save(commit=False)
+        profile.id = uid
+        profile.save()
+
+        return render_json('modify profile success')
+    else:
+        return render_json(profile_form.errors, errors.FROM_VALID_ERROR)
+
+    # pass
 def upload_avatar(request):
     """头像上传"""
-    pass
+
+    #判断是否是post请求
+    if not request.method == "POST":
+        return render_json('request method error', errors.REQUEST_ERROR)
+
+    avatar = request.FILES.get('avatar')
+    uid = request.session.get('uid')
+    user = User.objects.get(id=uid)
+
+    logics.upload_avatar.delay(user, avatar)
+
+    return render_json('upload success')
+
+
+
 
